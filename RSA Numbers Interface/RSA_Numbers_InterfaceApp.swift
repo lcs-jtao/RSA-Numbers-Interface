7d2acb1cf439c1173130938584493bf5542b82d2
Joyce Tao <jtao@lcs.on.ca>
2022-04-23 11:59:29 -0400

    Initial Commit

---
+++ RSA Numbers Interface/RSA_Numbers_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSA_Numbers_InterfaceApp.swift
+//  RSA Numbers Interface
+//
+//  Created by Joyce Tao on 2022-04-23.
+//
+
+import SwiftUI
+
+@main
+struct RSA_Numbers_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
