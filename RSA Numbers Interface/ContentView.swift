afceaa79a9eccfc3118de0ffd01ab4f28257302b
Joyce Tao <jtao@lcs.on.ca>
2022-04-24 17:29:34 -0400

    Created the static version of the main interface

--- RSA Numbers Interface/ContentView.swift
+++ RSA Numbers Interface/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var lowerBound = "1"
+    @State var upperBound = "1"
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Enter the lower limit of range")
+            TextField("Lower Limit", text: $lowerBound, prompt: Text("Enter integer from 1 to 1000"))
+            Text("Enter the upper limit of range")
+            TextField("Upper Limit", text: $upperBound, prompt: Text("Enter integer from 1 to 1000"))
+            Divider()
+            Text("The number of RSA numbers between \(lowerBound) and \(upperBound) is 0")
+        }
+        .padding()
     }
 }
 
